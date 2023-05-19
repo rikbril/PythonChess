3e742224f30208e2a6d0d955d09401037db57fb5
rikbril <riik.b@hotmail.com>
2023-05-19 14:45:57 +0200

    finished calculating the possible moves of each pieces and keeping track of which positions are pinned by the opposing colour

--- Main.py
+++ Main.py
@@ -8,8 +8,8 @@ df = pd.DataFrame(index=range(8), columns=range(8))
 for row in range(len(df.index)):
     for column in df.columns:
         df.iloc[row, column] = 0
-df_white = df.copy(deep=False)
-df_black = df.copy(deep=False)
+df_pinned_by_white = df.copy()
+df_pinned_by_black = df.copy()
 
 # function to create the startboard for easier testing of different starting positions
 def createBoard():
@@ -19,11 +19,12 @@ def createBoard():
         dictionary: contains the list of all the pieces, with as keys their name and the class they belong to
     """
     chess_pieces = {}
-    chess_pieces_location = {}
     chess_pieces_name_counter = {}
 
     start_fen = "RNBQKBNR/PPPPPPPP/8/8/8/8/pppppppp/rbnqkbnr"
-    start_fen = "8/8/8/3q4/8/8/8/8"
+    
+    start_fen = "RNBQKBNR/PPPPPPPP/8/8/8/8/8/rbnqkbnr"
+    #start_fen = "8/8/1q6/8/4P3/8/8/1p6"
 
     class_dictionary = {"R": Pieces.Rook, "N": Pieces.Knight, "B": Pieces.Bischop, "Q": Pieces.Queen, "K": Pieces.King, "P": Pieces.Pawn}
     class_spelled_out = {"R": "Rook", "N": "Knight", "B":"Bischop", "Q": "Queen", "K": "King", "P":"Pawn"}
@@ -43,7 +44,7 @@ def createBoard():
                 class_name += "Black"
             else:
                 class_name += "White"
-            location = [count//8], [count%8]
+            location = count//8, count%8
             if letter in chess_pieces_name_counter:
                 chess_pieces_name_counter[letter] += 1
             else:
@@ -52,23 +53,52 @@ def createBoard():
 
             df.iloc[location] = name
             chess_pieces[name] = class_dictionary[letter.capitalize()](name, location, is_white)
-            chess_pieces_location[name] = location
 
             count += 1
-    return chess_pieces, chess_pieces_location
+    return chess_pieces
 
 def Test():
-    print(chess_pieces[piece].currentLocation)
     result = Moves.getLocationFromName(chess_pieces, piece)
+    print(chess_pieces[piece].currentLocation)
+    print(chess_pieces[piece].is_white)
+    print(chess_pieces[piece].location)
 
 if __name__ == "__main__":
-    chess_pieces, chess_pieces_location = createBoard()
+    chess_pieces = createBoard()
     for piece in chess_pieces:
         if piece == "QueenWhite1":
             result = Moves.getLocationFromName(chess_pieces, piece)
-            print(chess_pieces[piece].movementDirections)
-            #print(df)
-
-            print(chess_pieces[piece].is_white)
-            print(chess_pieces[piece].location)
+            #print(chess_pieces[piece].movementDirections)
             
+            movement_result, pinned_result = Moves.calculatePossibleMoves(chess_pieces, piece)
+
+            chess_pieces[piece].movement = movement_result
+            chess_pieces[piece].pinned = pinned_result
+
+            for x in range (0,3):
+                Moves.modifyPinnedDF(chess_pieces, piece, df_pinned_by_white, df_pinned_by_black)
+
+            Moves.modifyPinnedDF(chess_pieces, piece, df_pinned_by_white, df_pinned_by_black, substract=True)
+
+            print(df_pinned_by_white)
+
+        
+        elif piece == "QueenWhite2":
+
+        
+            result = Moves.getLocationFromName(chess_pieces, piece)
+            #print(chess_pieces[piece].movementDirections)
+            
+            movement_result, pinned_result = Moves.calculatePossibleMoves(chess_pieces, piece)
+
+            chess_pieces[piece].movement = movement_result
+            chess_pieces[piece].pinned = pinned_result
+
+            if "White" in piece:
+                for direction in pinned_result:
+                    for step in direction:
+                        df_pinned_by_white.iloc[*step] += 1
+            else:
+                for direction in pinned_result:
+                    for step in direction:
+                        df_pinned_by_black.iloc[*step] += 1
--- Moves.py
+++ Moves.py
@@ -1,14 +1,169 @@
-def getKeyFromDict(dictionary, value):
-    for key in dictionary:
-        if key == value:
-            return dictionary[key]
-    return False
+def locationToColour(dict, value):
+    for item in dict:
+        if [*dict[item].location] == value:
+            return dict[item].is_white
+    return None
 
 def getLocationFromName(dict, value):
+    """unnesesary function, though i needed this function to acces class values from a memory location.
+       However, this can be done directly. will remove this function in the next commit
+
+    Args:
+        dict (_type_): _description_
+        value (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     for key in dict:
         if key == value:
             return dict[key].currentLocation
     
+def calculatePossibleMoves(dict, piece):
+    """Takes in a piece and calculates to which places the piece can move, and which locations are pinned by this piece. 
+    in order to keep track of attack/defend vectors (i presume, i dont play chess)
 
-def calculatePossibleMoves():
-    pass
+    This function uses 2 child functions: 
+     - directionsWithLocations: returns a nested array of all the places the piece can move to
+     - directionCounter: takes in the nested array which is provided by directionsWithLocations and per direction looks
+     at which pieces it encounters.
+
+    Args:
+        dict (dict): Dictionary of all the chess pieces and their memory adress of their class instance
+        piece (dict[key]): The name of the piece which is going to be used to calculate their possible moves.
+
+    Returns:
+        nested array: a nested array for each direction with the corresponding locations as steps.
+        Both for the possible movements of the piece and which pieces are pinned down.
+    """
+
+    single_move = dict[piece].single_move
+    is_white = dict[piece].is_white
+    row, column = dict[piece].location
+    
+    directions_with_locations = directionsWithLocations(dict, piece, single_move, row, column)
+    movement_result, pinned_result = directionCounter(dict, is_white, directions_with_locations)
+
+    return movement_result, pinned_result
+ 
+def directionCounter(dict, is_white, directions_with_locations):
+    """ Takes in a nested array of each direction a piece can move into and the location of the steps in each direction
+    Args:
+        dict (dict): dictionary of all the pieces
+        is_white (bool): color of the piece
+        directions_with_locations (nested array): array of each direction the piece may move to.
+        an array of all the steps in a direction is nested within
+
+    Returns:
+        nested array: a nested array for each direction with the corresponding locations as steps.
+        Both for the possible movements of the piece and which pieces are pinned down.
+    """
+
+    movement_result = []
+    pinned_result = []
+    for direction in directions_with_locations:
+        direction_movement_result = []
+        direction_pinned_result = []
+        encounter = 2
+        for step in direction:
+            previous_encounter = encounter
+            encounter_color = locationToColour(dict, step)
+            if encounter_color == None:
+                pass
+            elif encounter_color == is_white:
+                encounter -= 2
+            else:
+                encounter -= 1
+
+            if encounter == 2:
+                direction_movement_result.append(step)
+            if encounter >= 0 and previous_encounter > 1:
+                direction_pinned_result.append(step)
+
+            if encounter <= 0:
+                break
+        movement_result.append(direction_movement_result)
+        pinned_result.append(direction_pinned_result)
+    return movement_result, pinned_result
+
+def directionsWithLocations(dict, piece, single_move, row, column):
+    """transforms a list which contains all the directions a piece can move into a nested array of all the steps, per direction
+
+    Args:
+        dict (dict): dictionary of all the pieces
+        piece (dict[key]): name of the piece
+        single_move (bool): Breaks the direction loop if the piece can only move 1 spot
+        row (int): DF row of the piece
+        column (int): DF column of the piece
+
+    Returns:
+        _type_: _description_
+    """
+    
+    movement_directions = dict[piece].movementDirections
+    result = []
+
+    for direction in movement_directions:
+        step_counter = 1
+        rowAdjuster, columnAdjuster = direction
+        directionResult = []
+        while True:
+            adjustedRow = row + (rowAdjuster * step_counter)
+            adjustedColumn = column + (columnAdjuster * step_counter)
+
+            if adjustedRow < 0 or adjustedRow > 7:
+                break
+            if adjustedColumn < 0 or adjustedColumn > 7:
+                break
+
+            directionResult.append([adjustedRow, adjustedColumn])
+            
+            if single_move == True:
+                break
+            
+            step_counter += 1
+        
+        result.append(directionResult)
+
+    return result
+
+def modifyPinnedDF(dict, piece, df_pinned_by_white, df_pinned_by_black, substract=False):
+    pinned_result = dict[piece].pinned
+    modifier = 1
+
+    if substract == True:
+        modifier = -1
+
+    if "White" in piece:
+        for direction in pinned_result:
+            for step in direction:
+                df_pinned_by_white.iloc[*step] += modifier
+    else:
+        for direction in pinned_result:
+            for step in direction:
+                df_pinned_by_black.iloc[*step] += modifier
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
